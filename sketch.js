5b3e70e2e6cae70dbf4cf5391f0839f564337063
KiranK9876 <85800171+KiranK9876@users.noreply.github.com>
2021-07-12 13:41:01 -0500

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,21 @@
+var box1
+
+
+
+function setup(){
+  
+  createCanvas(400, 400);
+
+  box1=new Box(200,200,20,20,2,4)
+}
+
+function draw() 
+{
+  background(220);
+
+  box1.show()
+  box1.move()
+  box1.moveUp()
+
+}
+
